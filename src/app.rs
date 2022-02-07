dec0ce29ce2d664816eab06fa46648316cf09998
nsk6 <nsk@axofinans.no>
2022-02-07 17:10:58 +0100

    Split game logic from display logic

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,17 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
 use yew::prelude::*;
 
-mod tile;
-use tile::Tile;
+mod game;
+use game::*;
+use game::tile::Tile;
 
 pub enum Msg {
-    ClickTile(u8, u8),
+    ClickTile(u8),
     Restart,
 }
 
 pub struct App {
-    board: Vec<Tile>,
-    width: u8,
-    height: u8,
-    solved: bool,
+    game: Game,
     tile_size: u32,
-    move_count: u32
 }
 
 impl Component for App {
@@ -27,46 +22,35 @@ impl Component for App {
         let width = 4;
         let height = 4;
 
-        let mut new = App {
-            board: Vec::new(),
-            width: width,
-            height: height,
-            solved: false,
-            tile_size: 100,
-            move_count: 0
-        };
-
-        let order = App::random_legal_order(width, height, false);
+        let order = Game::random_legal_order(width, height, false);
+        let mut board: Vec<Tile> = Vec::new();
 
         let max_value = order.len();
         for i in order {
-            new.board.push(Tile::new(i, i == max_value as u8));
+            board.push(Tile::new(i, i == max_value as u8));
         }
 
-        new
+        App {
+            game: Game {
+                board: board,
+                width: width,
+                height: height,
+                solved: false,
+                move_count: 0
+            },
+
+            tile_size: 100,
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::ClickTile(index, value) => {
-                log::info!("Clicked {:?}", self.board[index as usize]);
-
-                if self.is_legal_move(index) && !self.solved {
-                    self.make_move(index);
-
-                    let order: Vec<u8> = self.board.iter().map(|tile| tile.id).clone().collect();
-                    self.solved = App::is_solved(&order);
-                }
-
-                log::info!("Is legal move: {}", self.is_legal_move(index));
-
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
+            Msg::ClickTile(index) => {
+                self.game.click_tile(index)
             }
+
             Msg::Restart => {
-                self.restart();
-                true
+                self.game.restart()
             },
         }
     }
@@ -80,12 +64,11 @@ impl Component for App {
             <h1>{"15 puzzle"}</h1>
                 <div class = "parent flexbox">
                     {
-                        for self.board.iter().enumerate().map(|(i, tile)|
+                        for self.game.board.iter().enumerate().map(|(i, tile)|
                         html! {
                             <a class = "child" onclick={
                                 let index = i.clone();
-                                let value = tile.id.clone();
-                                link.callback(move |_| Msg::ClickTile(index as u8, value as u8))
+                                link.callback(move |_| Msg::ClickTile(index as u8))
                             }>
                             {tile.view()}
                             </a>
@@ -94,7 +77,7 @@ impl Component for App {
                     }
                 </div>
                 <div id="stats">
-                <p> {self.move_count} </p>
+                <p> {self.game.move_count} </p>
                 </div>
                 <div id="restart">
                     <button onclick={link.callback(|_| Msg::Restart)} >{ "Restart" }</button>
@@ -107,7 +90,7 @@ impl Component for App {
 impl App {
 
     fn style(&self) -> Html {
-        let visible = match self.solved {
+        let visible = match self.game.solved {
             true => "visible",
             false => "hidden",
         };
@@ -143,8 +126,8 @@ impl App {
                 }}
 
             ", 
-            parent_width = self.width,
-            parent_height = self.height as u32 * self.tile_size,
+            parent_width = self.game.width,
+            parent_height = self.game.height as u32 * self.tile_size,
             child_width = self.tile_size,
             child_height = self.tile_size,
             visibility = visible,
@@ -152,144 +135,5 @@ impl App {
             </style>
             </>
         }
-    }
-
-    fn random_legal_order(width: u8, height: u8, real: bool) -> Vec<u8> {
-        let mut order: Vec<u8> = Vec::new();
-
-        for i in 1..(width * height + 1) {
-            order.push(i);
-        }
-
-        if real {
-        let mut rng = thread_rng();
-        order.shuffle(&mut rng);
-
-        while !App::is_legal_order(&order, width, height) && !App::is_solved(&order) {
-            order.shuffle(&mut rng);
-        }
-    }
-
-        order
-    }
-
-    fn is_legal_order(order: &Vec<u8>, width: u8, height: u8) -> bool {
-        let mut inversions = 0;
-
-        let mut blank_index = 0;
-
-        for i in 0..order.len() {
-            if order[i] as usize == order.len() {
-                blank_index = i as u8;
-                continue;
-            }
-            for j in i..order.len() {
-                if order[i] > order[j] {
-                    inversions += 1;
-                }
-            }
-        }
-
-        let blank_layer = height - ((blank_index / width) + 1);
-
-        log::info!("Inversions {} | Blank layer {}", inversions, blank_layer);
-
-        return (inversions % 2 == 0) != (blank_layer % 2 == 0);
-    }
-
-    fn is_solved(order: &Vec<u8>) -> bool {
-        for (i, value) in order.iter().enumerate() {
-            if &(i as u8 + 1) != value {
-                return false;
-            }
-        }
-
-        log::info!("Puzzle is solved!");
-        true
-    }
-
-    fn is_legal_move(&self, index_clicked: u8) -> bool {
-        let empty_pos = self.empty_index();
-
-        if index_clicked == empty_pos {
-            return false;
-        }
-
-        // TODO: Fix moves wrapping lines
-
-        if index_clicked != 0 {
-            let target = self.board.get(index_clicked as usize - 1);
-            match target {
-                Some(tile) => {
-                    if tile.empty {
-                        return true;
-                    }
-                }
-                None => {}
-            }
-        }
-
-        let target = self.board.get(index_clicked as usize + 1);
-        match target {
-            Some(tile) => {
-                if tile.empty {
-                    return true;
-                }
-            }
-            None => {}
-        }
-
-        if index_clicked >= self.width {
-            let target = self.board.get(index_clicked as usize - self.width as usize);
-            match target {
-                Some(tile) => {
-                    if tile.empty {
-                        return true;
-                    }
-                }
-                None => {}
-            }
-        }
-
-        let target = self.board.get(index_clicked as usize + self.width as usize);
-        match target {
-            Some(tile) => {
-                if tile.empty {
-                    return true;
-                }
-            }
-            None => {}
-        }
-
-        false
-    }
-
-    fn make_move(&mut self, index_clicked: u8) {
-        let empty = App::empty_index(self);
-
-        self.board.swap(empty as usize, index_clicked as usize);
-        self.move_count += 1;
-    }
-
-    fn empty_index(&self) -> u8 {
-        for (i, tile) in self.board.iter().enumerate() {
-            if tile.empty {
-                return i as u8;
-            }
-        }
-        return 0;
-    }
-
-    fn restart(&mut self) {
-        let new_order = App::random_legal_order(self.width, self.height, true);
-
-        let max_value = new_order.len() - 1;
-
-        for i in 0..max_value {
-            self.board[i] = Tile::new(new_order[i], i as u8 == max_value as u8)
-        }
-
-        self.solved = false;
-        self.move_count = 0;
     }
 }
---
+++ src/app/game.rs
@@ -0,0 +1,171 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+pub mod tile;
+use tile::Tile;
+
+pub struct Game {
+    pub board: Vec<Tile>,
+    pub width: u8,
+    pub height: u8,
+    pub solved: bool,
+    pub move_count: u32,
+}
+
+impl Game {
+    pub fn random_legal_order(width: u8, height: u8, real: bool) -> Vec<u8> {
+        let mut order: Vec<u8> = Vec::new();
+
+        for i in 1..(width * height + 1) {
+            order.push(i);
+        }
+
+        if real {
+        let mut rng = thread_rng();
+        order.shuffle(&mut rng);
+
+        while !Game::is_legal_order(&order, width, height) && !Game::is_solved(&order) {
+            order.shuffle(&mut rng);
+        }
+    }
+
+        order
+    }
+
+    fn is_legal_order(order: &Vec<u8>, width: u8, height: u8) -> bool {
+        let mut inversions = 0;
+
+        let mut blank_index = 0;
+
+        for i in 0..order.len() {
+            if order[i] as usize == order.len() {
+                blank_index = i as u8;
+                continue;
+            }
+            for j in i..order.len() {
+                if order[i] > order[j] {
+                    inversions += 1;
+                }
+            }
+        }
+
+        let blank_layer = height - ((blank_index / width) + 1);
+
+        log::info!("Inversions {} | Blank layer {}", inversions, blank_layer);
+
+        return (inversions % 2 == 0) != (blank_layer % 2 == 0);
+    }
+
+    fn is_solved(order: &Vec<u8>) -> bool {
+        for (i, value) in order.iter().enumerate() {
+            if &(i as u8 + 1) != value {
+                return false;
+            }
+        }
+
+        log::info!("Puzzle is solved!");
+        true
+    }
+
+    fn is_legal_move(&self, index_clicked: u8) -> bool {
+        let empty_pos = self.empty_index();
+
+        if index_clicked == empty_pos {
+            return false;
+        }
+
+        // TODO: Fix moves wrapping lines
+
+        if index_clicked != 0 {
+            let target = self.board.get(index_clicked as usize - 1);
+            match target {
+                Some(tile) => {
+                    if tile.empty {
+                        return true;
+                    }
+                }
+                None => {}
+            }
+        }
+
+        let target = self.board.get(index_clicked as usize + 1);
+        match target {
+            Some(tile) => {
+                if tile.empty {
+                    return true;
+                }
+            }
+            None => {}
+        }
+
+        if index_clicked >= self.width {
+            let target = self.board.get(index_clicked as usize - self.width as usize);
+            match target {
+                Some(tile) => {
+                    if tile.empty {
+                        return true;
+                    }
+                }
+                None => {}
+            }
+        }
+
+        let target = self.board.get(index_clicked as usize + self.width as usize);
+        match target {
+            Some(tile) => {
+                if tile.empty {
+                    return true;
+                }
+            }
+            None => {}
+        }
+
+        false
+    }
+
+    fn make_move(&mut self, index_clicked: u8) {
+        let empty = Game::empty_index(self);
+
+        self.board.swap(empty as usize, index_clicked as usize);
+        self.move_count += 1;
+    }
+
+    fn empty_index(&self) -> u8 {
+        for (i, tile) in self.board.iter().enumerate() {
+            if tile.empty {
+                return i as u8;
+            }
+        }
+        return 0;
+    }
+
+    pub fn click_tile(&mut self, index: u8) -> bool {
+        log::info!("Clicked {:?}", self.board[index as usize]);
+
+        if self.is_legal_move(index) && !self.solved {
+            self.make_move(index);
+
+            let order: Vec<u8> = self.board.iter().map(|tile| tile.id).clone().collect();
+            self.solved = Game::is_solved(&order);
+        }
+
+        log::info!("Is legal move: {}", self.is_legal_move(index));
+
+        true
+    }
+
+    pub fn restart(&mut self) -> bool {
+        let new_order = Game::random_legal_order(self.width, self.height, true);
+
+        let max_value = new_order.len() - 1;
+
+        for i in 0..max_value {
+            self.board[i] = Tile::new(new_order[i], i as u8 == max_value as u8)
+        }
+
+        self.solved = false;
+        self.move_count = 0;
+
+        true
+    }
+}
